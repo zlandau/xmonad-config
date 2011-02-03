6e02effd38d91f10be039ed52179c8fa536c6c82
Zachary Landau <zachary.landau@palm.com>
2011-02-03 11:34:18 -0800

    Added support for multiple monitors

--- xmonad.hs
+++ xmonad.hs
@@ -1,26 +1,44 @@
 import XMonad
-import XMonad.Config.Gnome
-import qualified Data.Map as M
+import XMonad.Actions.PhysicalScreens
 import XMonad.Actions.SinkAll
-import XMonad.Util.Scratchpad
+import XMonad.Config.Gnome
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.DwmStyle
+import XMonad.Layout.IndependentScreens
+import XMonad.Util.Scratchpad
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
-myKeys conf@(XConfig {XMonad.modMask = modMask}) =
-    [ ((modMask .|. shiftMask, xK_t     ), sinkAll)
-    , ((modMask,               xK_w     ), scratchpadSpawnAction conf)
+myWorkspaces = ["1", "2", "3", "4", "5"]
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm .|. shiftMask, xK_t     ), sinkAll)
+    , ((modm,               xK_w     ), scratchpadSpawnAction conf)
     ]
+    ++
 
-newKeys x = M.union (keys gnomeConfig x) (M.fromList (myKeys x))
+    -- Don't switch monitors when selecting view
+    [((m .|. modm, k), windows $ onCurrentScreen f i)
+             | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
+             , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+
+    -- Name screens by physical location, not ScreenID
+    [((m .|. modm, k), f sc)
+            | (k, sc) <- zip [xK_w, xK_e] [0..]
+            , (f, m) <- [(viewScreen, 0), (sendToScreen, shiftMask)]] 
+
+newKeys x = M.union (myKeys x) (keys gnomeConfig x)
 
 myManageHook = scratchpadManageHookDefault <+> manageDocks
 
 main = do
-	xmonad $ gnomeConfig {
-		modMask = mod4Mask,
-		focusedBorderColor = "blue",
-        workspaces = ["code", "web", "comm"] ++ map show [4..9],
-        keys = newKeys,
-        manageHook = myManageHook,
-        layoutHook = dwmStyle shrinkText defaultTheme (layoutHook gnomeConfig)
-	}
+  nScreens <- countScreens
+  xmonad $ gnomeConfig {
+       modMask = mod4Mask,
+       focusedBorderColor = "blue",
+       workspaces = withScreens nScreens myWorkspaces,
+       keys = newKeys,
+       manageHook = myManageHook,
+       layoutHook = dwmStyle shrinkText defaultTheme (layoutHook gnomeConfig)
+  }
